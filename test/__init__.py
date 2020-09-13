f955ed42eafb86e1db9f4e2142cdbf25a655d333
Ristirianto Adi <ristiriantoadi@gmail.com>
2020-09-13 08:12:26 +0800

    flask pymongo

--- test/__init__.py
+++ test/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='49e244cf65248712dc2ce77e4196f012'
+
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
 db = SQLAlchemy(app)
+
+app.config["MONGO_URI"] = "mongodb://localhost:27017/test"
+mongo = PyMongo(app)
 
 from test import routes
--- test/routes.py
+++ test/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template,url_for,flash,redirect
-from test import app
+from test import app, mongo
 from .models import User,Post
 from .forms import RegistrationForm,LoginForm
 
@@ -42,3 +42,57 @@ def halo():
 def show_user_profile(username):
     # show the user profile for that user
     return 'My name is '+str(username)
+
+@app.route('/user/add')
+def add_user():
+    userCollection = mongo.db.user
+    userCollection.insert({'name':'harry potter','password':'123','gender':'boy'})
+    userCollection.insert({'name':'remus lupin','password':'111'})
+    return "add a user"
+
+@app.route('/user/find')
+def find_user():
+    userCollection = mongo.db.user
+    # userCollection.insert({'name':'risti','password':'123','gender':'boy'})
+    user=userCollection.find_one({'name':'risti'})
+    # return "{user['name']}"
+    # app.logger.info('logged in successfully')
+    return user['name']+" "+user['gender']
+
+@app.route('/user/find-many')
+def find_users():
+    userCollection = mongo.db.user
+    # userCollection.insert({'name':'risti','password':'123','gender':'boy'})
+    users=userCollection.find({'gender':'boy'})
+    # for user in users:
+    #     app.logger.info(user['name'])
+    return render_template("users.html",users=users)
+    # return "{user['name']}"
+    # return user['name']+" "+user['gender']
+
+@app.route('/user/update')
+def update_user():
+    userCollection = mongo.db.user
+    # userCollection.insert({'name':'risti','password':'123','gender':'boy'})
+    user=userCollection.find_one({'name':'risti'})
+    # return "{user['name']}"
+    user['rumah'] = "home"
+    user['password']='111'
+    user['usia'] += 2
+    userCollection.save(user)
+    return "updated a user"
+
+@app.route('/user/delete')
+def delete_user():
+    userCollection = mongo.db.user
+    user=userCollection.find_one({'name':'risti'})
+    userCollection.remove(user)
+    return "remove "+str(user['name'])
+
+#this one doesnt work
+# @app.route('/user/delete-many')
+# def delete_users():
+#     userCollection = mongo.db.user
+#     users=userCollection.find({'password':'123'})
+#     userCollection.deleteMany(users)
+#     return "remove many users"
